cb0a14af247370ffb26b5a98a0af32cac7f75822
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2025-12-29 18:26:56 +0900

    Update link-checker.rb

--- Formula/link-checker.rb
+++ Formula/link-checker.rb
@@ -1,22 +1,22 @@
 class LinkChecker < Formula
     homepage 'https://github.com/koba-e964/link-checker'
-    version '0.0.1'
+    version '0.0.2'
 
     if OS.mac?
         if Hardware::CPU.arm?
-            url 'https://github.com/koba-e964/link-checker/releases/download/v0.0.1/link-checker_v0.0.1_darwin_arm64.zip'
-            sha256 '7587944a32bdd43f8e678ad1d44f00a3089d22a6cc9d6caa69a8b5eeeb3e0f03'
+            url 'https://github.com/koba-e964/link-checker/releases/download/v0.0.2/link-checker_v0.0.2_darwin_arm64.zip'
+            sha256 '5e5ae4af25445e8dce0d60cf4f733650abf9e02c543b2f6979cb3ab656e070f0'
         elsif Hardware::CPU.intel?
-            url 'https://github.com/koba-e964/link-checker/releases/download/v0.0.1/link-checker_v0.0.1_darwin_amd64.zip'
-            sha256 '32e0df02f66ccbdbe2d7032b8b316a6420e7bf5521fe2eb576ebb25caa13164c'
+            url 'https://github.com/koba-e964/link-checker/releases/download/v0.0.2/link-checker_v0.0.2_darwin_amd64.zip'
+            sha256 '684c79e94af2d1566bae277b761382edf113c06f3afbdd09a851f3c938d0a44b'
         end
     elsif OS.linux?
         if Hardware::CPU.arm?
-            url 'https://github.com/koba-e964/link-checker/releases/download/v0.0.1/link-checker_v0.0.1_linux_arm64.tar.gz'
-            sha256 'ccf55975f579d1d6974383cc799074452a5754179326929bc3b3672bf543683d'
+            url 'https://github.com/koba-e964/link-checker/releases/download/v0.0.2/link-checker_v0.0.2_linux_arm64.tar.gz'
+            sha256 'd9c1e3553fac1bbc09e20d7a385a4b7bcf0e9f4d335510c41fa2c9787df4ead8'
         elsif Hardware::CPU.intel?
-            url 'https://github.com/koba-e964/link-checker/releases/download/v0.0.1/link-checker_v0.0.1_linux_amd64.tar.gz'
-            sha256 'bca7bd40e1e6b97baeb016c61a46692359aaf3f6014f7c4bb54b9d3b6b802da5'
+            url 'https://github.com/koba-e964/link-checker/releases/download/v0.0.2/link-checker_v0.0.2_linux_amd64.tar.gz'
+            sha256 '2d098c8aac568b1ae475680fe0d0600d05435aaa8823cd3e8d61848016313881'
         end
     end
 
